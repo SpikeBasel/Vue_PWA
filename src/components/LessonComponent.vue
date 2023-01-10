8fb5c6168461ac968d9afc299afaca11028f6d48
SpikeBasel <spikerine@gmail.com>
2023-01-10 16:42:26 +0500

    Added Template Data for Lesson component

--- src/components/LessonComponent.vue
+++ src/components/LessonComponent.vue
@@ -1,6 +1,17 @@
 <!-- eslint-disable vue/valid-template-root -->
 <template>
-
+  <div>
+    <section class="lessons">
+      <p class="error" v-if="error">{{ error }}</p>
+      <div v-for="(lesson, index) in lessons" v-bind:item="lesson" v-bind:index="index" v-bind:key="lesson._id" class="lesson" >
+          <ul class="lesson-text">
+            <li> topic: {{ lesson.topic }} </li>
+            <li> location: {{ lesson.location }} </li>
+            <li> price: {{ lesson.price }} </li>
+          </ul>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
