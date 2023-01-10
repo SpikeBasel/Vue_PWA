67b6423467c4d86163c3970103516d7f8d7e9f0b
SpikeBasel <spikerine@gmail.com>
2023-01-10 16:41:05 +0500

    Added Lesson component

---
+++ src/components/LessonComponent.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+
+</template>
+
+<script>
+import LessonService from '../LessonService';
+
+export default {
+
+  name: 'LessonComponent',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  box-sizing: border-box;
+  font-weight:bold;
+  font-size: 25px;
+  margin: 5px;
+}
+
+</style>
