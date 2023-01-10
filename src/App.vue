fbfea101475607446938774656e0ca81a5758072
SpikeBasel <spikerine@gmail.com>
2023-01-10 16:36:38 +0500

    Added gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LessonComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LessonComponent from './components/LessonComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LessonComponent
   }
 }
 </script>
