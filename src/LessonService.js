58b6f65c0f70e6d36073ad98f3bd8447c7ff6020
SpikeBasel <spikerine@gmail.com>
2023-01-10 16:38:14 +0500

    Added GET path with promise

--- src/LessonService.js
+++ src/LessonService.js
@@ -3,6 +3,22 @@ const url = 'http://localhost:5000/lessons/';
 
 class LessonService {
 
+    // Get Lessons
+    static getLessons() {
+      // eslint-disable-next-line no-async-promise-executor
+      return new Promise(async (resolve, reject) => {
+        try {
+          const res = await fetch(url);
+          const data = res.json();
+          resolve(
+            data
+          );
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+ 
   }
   
   export default LessonService;
